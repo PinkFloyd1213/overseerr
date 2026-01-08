db22a8bb5f570d82b2cb381abe3682d074bcfde3
PinkFloyd1213 <ilian@taini.ch>
2026-01-08 01:49:51 +0100

    feat: Add enableDeletionRequests setting with default false

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -242,6 +242,7 @@ CoreApp.getInitialProps = async (initialProps) => {
     locale: 'en',
     emailEnabled: false,
     newPlexLogin: true,
+    enableDeletionRequests: false,
   };
 
   if (ctx.res) {
